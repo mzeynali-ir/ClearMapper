46295a29aac1cfd3874f37ab5115b22fd03dd4ce
Mostafa Zeynali <121800106+mzeynali-ir@users.noreply.github.com>
2024-06-25 20:10:33 +0330

    ref foldering

--- ClearMapper/ClearMapper.cs
+++ ClearMapper/ClearMapper.cs
@@ -67,48 +67,6 @@ namespace ClearMapperLibrary
         }
 
     }
-
-
-    public partial class ClearMapper
-    {
-
-        public IEnumerable<TDestination> Map<TSource, TDestination>(IEnumerable<TSource> source)
-            where TSource : class
-            where TDestination : class
-        {
-
-            var config = findConfig<TSource, TDestination>();
-            var destination = source.Select(config);
-
-            return destination;
-
-        }
-
-        public IQueryable<TDestination> Map<TSource, TDestination>(IQueryable<TSource> source)
-             where TSource : class
-             where TDestination : class
-        {
-
-            var config = findConfig<TSource, TDestination>();
-            var destination = source.Select(x => config(x));
-
-            return destination;
-
-        }
-
-        public TDestination Map<TSource, TDestination>(TSource source)
-            where TSource : class
-            where TDestination : class
-        {
-
-            var config = findConfig<TSource, TDestination>();
-            var destination = config(source);
-
-            return destination;
-
-        }
-
-    }
 }
 
 
---
+++ ClearMapper/Methods/Map.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClearMapperLibrary
+{
+    public partial class ClearMapper
+    {
+
+        public IEnumerable<TDestination> Map<TSource, TDestination>(IEnumerable<TSource> source)
+            where TSource : class
+            where TDestination : class
+        {
+
+            var config = findConfig<TSource, TDestination>();
+            var destination = source.Select(config);
+
+            return destination;
+
+        }
+
+        public IQueryable<TDestination> Map<TSource, TDestination>(IQueryable<TSource> source)
+             where TSource : class
+             where TDestination : class
+        {
+
+            var config = findConfig<TSource, TDestination>();
+            var destination = source.Select(x => config(x));
+
+            return destination;
+
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source)
+            where TSource : class
+            where TDestination : class
+        {
+
+            var config = findConfig<TSource, TDestination>();
+            var destination = config(source);
+
+            return destination;
+
+        }
+
+    }
+}
+
+
